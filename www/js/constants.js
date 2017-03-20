a7fffc675b6e6e769fe14f169e38dec5ec12c038
Xavier <xavier.aeby@outlook.com>
2017-03-20 15:48:14 +0100

    modif

--- www/js/constants.js
+++ www/js/constants.js
@@ -1,10 +1,5 @@
 angular.module('citizen-engagement')
   .constant('apiUrl', '/api-proxy')
-<<<<<<< HEAD
   .constant('mapboxSecret', 'pk.eyJ1IjoieGF2aWp1bmlvciIsImEiOiJjaXZjYmplejEwMGNkMm9vMGx5c2xqbTB4In0.bkX2UN2l0Bgy8lwEMeO-_A')
-=======
-  .constant('mapboxSecret', 'undefined')
   .constant('qimgUrl', '@qimgUrl@')
-  .constant('qimgSecret', '@qimgSecret@')
->>>>>>> 5fa26ecd105b2a0392e057c372a1b8e154d3a5b4
-;
+  .constant('qimgSecret', '@qimgSecret@');
