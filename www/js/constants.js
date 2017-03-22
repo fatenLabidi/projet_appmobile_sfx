984b4125b0a9dd7e84ca2a6fbfa05883bfe31d10
Simon Contreras <simon.contreras@heig-vd.ch>
2017-03-22 15:10:02 +0100

    uniformisation du style

--- www/js/constants.js
+++ www/js/constants.js
@@ -1,8 +1,6 @@
 angular.module('citizen-engagement')
-  .constant('apiUrl', 'https://comem-citizen-engagement-2017f.herokuapp.com/api')
+  .constant('apiUrl', '/api-proxy')
   .constant('mapboxSecret', 'undefined')
-  .constant('qimgUrl', 'https://comem-qimg.herokuapp.com/api')
+  .constant('qimgUrl', '/qimg-api-proxy')
   .constant('qimgSecret', '2708ikdCZ0h5ss/6si5P1urmxLLA7ekKq3lE8JpdJjSE2PEVBvvUmbzaOR17Y/fvCT47vNPDZiJ99TVhBGKY2BUJQyGYSlwyBIP8T2njnsnnSqkRIhXlcZZXhrF3OTmKC+v7kJ7ffWRJNgm+lowaA/F+CIvVUFShFbewckBR+Yw=')
-  .constant('qimgSecret', '@qimgSecret@')
-
 ;
